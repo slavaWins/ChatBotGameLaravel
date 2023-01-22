33591b53c6fc28eaa01d85f2823d4ce0d383b7c1
Вячеслав Беляков <zxc7613@mail.ru>
2023-01-22 13:59:25 +0700

    prop

--- resources/views/mr-properter/migration.blade.php
+++ resources/views/mr-properter/migration.blade.php
@@ -8,8 +8,7 @@ use App\Library\MrProperter\MigrationRender;
 Schema::create('{{$tableName}}', function(Blueprint $table) {
 $table->id();@foreach($list as $ind=> $data)
 
-    $table @foreach( $data as $fun=> $arg)  ->{{$fun}}({{$arg}}) @endforeach
-@endforeach
+    $table @foreach( $data as $fun=> $arg)  ->{{$fun}}({{$arg}}) @endforeach;@endforeach
 
 $table->timestamps();
 });
